192145f8b2cc89552fec255b5d40a34207b5d5bd
ma1526 <magithublf@gmail.com>
2019-04-08 23:13:52 +0000

    lab4 commit

--- app_server/models/blogs.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var blogSchema = new mongoose.Schema({
- blogTitle: {type: String, required: true },
- blogText: {type: String, required: true},
- dateOfCreation: { type: Date, "default": Date.now }
-});
-
-mongoose.model('blog', blogSchema);
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -8,10 +8,15 @@ var ctrMain = require('../controllers/blog');
 
 router.get('/', ctrMain.index);
 router.get('/blogList', ctrMain.blogList);
-router.get('/blogAdd', ctrMain.blogAdd);
-router.get('/blogEdit', ctrMain.blogEdit);
-router.get('/blogDelete', ctrMain.blogDelete);
 
+router.get('/blogAdd', ctrMain.getBlogAdd);
+router.post('/blogAdd', ctrMain.postBlogAdd);
+
+router.get('/blogEdit/:blogid', ctrMain.getBlogEdit);
+router.post('/blogEdit/:blogid', ctrMain.postBlogEdit);
+
+router.get('/blogDelete/:blogid', ctrMain.getBlogDelete);
+router.post('/blogDelete/:blogid', ctrMain.postBlogDelete);
 
 var port = process.env.PORT || 80;
 module.exports = router;
