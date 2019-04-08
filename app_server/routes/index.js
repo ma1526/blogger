5612b070bfa6c2b3791d1d65068677e5efb2d5fe
ma1526 <magithublf@gmail.com>
2019-04-08 22:02:19 +0000

    first commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+///var ctrlMain = require('../controllers/blog.js');
+//var ctrlLocations = require('../controllers/locations');
+//var ctrlOthers = require('../controllers/others');
+var ctrMain = require('../controllers/blog');
+
+
+router.get('/', ctrMain.index);
+router.get('/blogList', ctrMain.blogList);
+router.get('/blogAdd', ctrMain.blogAdd);
+
+var port = process.env.PORT || 80;
+module.exports = router;
